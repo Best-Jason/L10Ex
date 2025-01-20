1cfd8223d45029222457a81bb52ce37c9a2bc765
Best-Jason <23022483@myrp.edu.sg>
2025-01-20 11:01:22 +0800

    fitering data and fetching data from api

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View} from 'react-native';
 
-export default function App() {
+let originalData = [];
+
+const App = () => {
+  const [mydata, setMydata] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums")
+    .then(response => {
+        return response.json(); //return in json format
+    })
+    .then((myjson) => {
+        if (originalData.length < 1)
+        {
+            setMydata(myjson); // mydata is myjson which from url data (this is data that changes)
+            originalData = myjson;// originalData is myjson which from url data (there is 2 data that is using data from url)(this is used as default data)
+        }
+    })
+  }, []);
+
+  const filterData = (text) => {
+      if (text != '') { //if data's title includes/has text from the search bar, it set the mydata to myFiltereddata which filtered originaldata without changing originaldata)
+          let myFilteredData = originalData.filter((item) =>
+          item.title.includes(text));
+          setMydata(myFilteredData);
+      }
+      else{
+          setMydata(originalData); //if nothing is added to search bar, it then uses original data
+      }
+  }
+
+  const renderItem = ({item, index}) => {
+    return (
+    <View>
+    <Text style={{borderWidth:1}}>{item.title}</Text>
+    </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar/>
+      <Text>Search:</Text>
+      <TextInput style={{borderWidth:1}} onChangeText={(text) =>{filterData(text)}} placeholder="Search..." />
+      <FlatList data={mydata} renderItem={renderItem} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
